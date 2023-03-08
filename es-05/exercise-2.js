ea13a974fb61b5dc9276df20971e985a6a8ef7fe
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-03-08 14:26:05 +0100

    Exercise 1 - Conditional and Loops

---
+++ es-05/exercise-2.js
@@ -0,0 +1,36 @@
+// ceo => 2200
+// manager => 1800
+// cto => 1800
+// developer => 1500
+// default => 1000
+function calculateSalary(role) {
+  switch (role) {
+    case "ceo":
+      return 2200;
+      break;
+    case "manager":
+      return 1800;
+      break;
+    case "cto":
+      return 1800;
+      break;
+    case "developer":
+      return 1500;
+      break;
+    case "other":
+      return 1000;
+      break;
+  }
+}
+
+const ceoSalary = calculateSalary("ceo");
+const managerSalary = calculateSalary("manager");
+const ctoSalary = calculateSalary("cto");
+const developerSalary = calculateSalary("developer");
+const otherSalary = calculateSalary("other");
+
+console.log(ceoSalary);
+console.log(managerSalary);
+console.log(ctoSalary);
+console.log(developerSalary);
+console.log(otherSalary);
