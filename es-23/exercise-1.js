c1cea26845763f28c2a26e335587325c43929727
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-03-09 12:05:44 +0100

    Exercise 9 - Objects Methods

---
+++ es-23/exercise-1.js
@@ -0,0 +1,9 @@
+const person = {
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+
+// Print values of person using Object.keys
+console.log(Object.keys(person) + " " + Object.values(person));
+
