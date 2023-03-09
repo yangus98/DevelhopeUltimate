6f590cab9cc673f7733227fcd638f91faf6e63a9
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-03-09 09:32:38 +0100

    Exercise 7 - Closures

---
+++ es-38/exercise.js
@@ -0,0 +1,11 @@
+function printName() {
+    let helloName= "Hello John";
+    function inner() {
+     setTimeout(() => {
+        console.log(helloName);
+     }, 1000)
+    }
+    return inner;
+}
+
+printName()();
