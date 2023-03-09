84e08abd08cbcdfb346d2bcd5154a33a330c472a
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-03-09 17:47:59 +0100

    Exercise 15 - Array Methods Advanced

---
+++ es-57/exercise-5.js
@@ -0,0 +1,15 @@
+const people = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+SortingPeople=people.sort((firstItem, secondItem) => firstItem.age - secondItem.age);
+console.log(SortingPeople)
